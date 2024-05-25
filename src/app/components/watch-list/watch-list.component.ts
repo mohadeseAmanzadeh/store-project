b156c8ebe2f12d1800198c9653e3ca2ba157aec1
mohadesea <amanzadehmohadese@gmail.com>
2024-05-25 14:28:07 +0330

    --

--- src/app/components/watch-list/watch-list.component.ts
+++ src/app/components/watch-list/watch-list.component.ts
@@ -2,38 +2,54 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app.service';
 import { CommonModule } from '@angular/common';
+import { SorterListComponent } from '../sorter-list/sorter-list.component';
+import { AccordionDirective } from '../accordion/accordion.directive';
+import { AccordionComponent } from '../accordion/accordion.component';
 
 
 @Component({
   selector: 'app-watch-list',
   standalone: true,
-  imports: [HttpClientModule,
-    CommonModule
+  imports: [
+    SorterListComponent,
+    HttpClientModule,
+    CommonModule,
+    AccordionDirective,
+    AccordionComponent,
   ],
   templateUrl: './watch-list.component.html',
   styleUrl: './watch-list.component.scss'
 })
 export class WatchListComponent implements OnInit{
 
-  public watchlist: any = [];
-  public list: any = [];
+  public watchList: any = [];
+  public brandList: any = [];
 
   constructor(
     private myService : AppService
   ) {}
 
   ngOnInit(): void {
-    this.myService.getData().subscribe((resp: any) => {
-      console.log(resp);
-      this.watchlist = resp
-    });
-
-    this.myService.getList().subscribe((resp: any) => {
-      console.log(resp);
-      this.list = resp
+    
+    this.myService.getList().subscribe((brandList: any) => {
+      this.myService.getData().subscribe((watchList: any) => {
+        this.watchList = this._prepareItems(watchList, brandList);
+      });
     });
   }
 
+  private _prepareItems(watchList: any, brandList: any) {
+    watchList.forEach((watch: any) => {
+      let idx = brandList.findIndex((brand: any) => brand.id == watch.brandId);
+      watch.brandTitle = brandList[idx].title;
+    });
+    return watchList;
+  }
 
+  // getBrand(brandId: number) {
+  //   let idx = this.brandList.findIndex((brand: any) => brand.id == brandId);
+  //   return this.brandList[idx];
+  // }
+  
   
 }
