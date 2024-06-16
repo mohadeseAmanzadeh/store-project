95d3895646ddda566d346a3c41776b2d526775fc
mohadesea <amanzadehmohadese@gmail.com>
2024-06-16 17:33:37 +0330

    bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SvgIconComponent } from 'angular-svg-icon';
 import { RouterOutlet} from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { AccordionDirective } from './components/accordion/accordion.directive';
+import { WatchListService } from './services/watch-list.service';
 
 @Component({
   selector: 'app-root',
@@ -25,6 +26,18 @@ import { AccordionDirective } from './components/accordion/accordion.directive';
     
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'store-project';
+
+  constructor(
+    private watchListService: WatchListService,
+) {} 
+
+  ngOnInit(): void {
+   
+    this.watchListService.getWatchList().then((resp: any) => {   
+        // this.list = resp;
+    }) 
+      
+  }
 }
