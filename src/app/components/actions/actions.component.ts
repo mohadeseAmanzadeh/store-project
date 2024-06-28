6aa2fccfac3804f72e891ef7f9f4a51756003a2b
unknown <amanzadehmohadese@gmail.com>
2024-06-28 13:19:33 -0700

    actions

---
+++ src/app/components/actions/actions.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-actions',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './actions.component.html',
+  styleUrl: './actions.component.scss'
+})
+export class ActionsComponent {
+ 
+}
